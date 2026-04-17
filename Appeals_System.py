2af622feb061f57a08749b3539bf711e6b95be48
jaafaralzubaidy <jaafarsafaa63@gmail.com>
2026-04-17 21:34:15 +0300

    Update Appeals_System.py

--- Appeals_System.py
+++ Appeals_System.py
@@ -25,11 +25,11 @@ def now_iraq():
     return datetime.now(IRAQ_TZ).strftime("%Y-%m-%d %H:%M:%S")
 
 KPI_LIST = [
-    "AHT", "ACW", "CSAT", "FCR", "Adherence",
-    "Quality Score", "Attendance", "SLA", "Other"
+    "Done Delay", "Done Delay Response", "High MTTD", "Shift Delay", "Ticket Not Add",
+    "Wrong Action", "Delay In q", "High ASR Utlization", "Reduce Number Of Incident", "Delay High Impact", "Zabbix No Match", "Closing Issue", "Wrong Forward", "Wrong Action In Q Manager", "FMS", "Delay FMS", " Number Delay FMS", "No Task"
 ]
 
-TAB_LIST = ["Calls", "Chats", "Emails", "Callbacks", "Other"]
+TAB_LIST = ["Bridges", "Earthlink Services", "IRQNBN", "Back Bone", "ITPC", "Metro", "Nas", "Power", "Baghdad Rings", "Server Room", "Switch State", "Wireless", "Al-watani Power", "Al-watani Services"]]
 
 # ───────────────────────────────────────────────
 # LOGGING SETUP
